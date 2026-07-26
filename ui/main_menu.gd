1ac25dc3021d598822a38b8c5558de8576fbd64f
buchegerb1704 <buchegerb1704@gmail.com>
2026-07-26 02:55:57 -0500

    Music on the main menu

--- ui/main_menu.gd
+++ ui/main_menu.gd
@@ -1,12 +1,80 @@
 extends Control
 
+@export var options_menu: TextureRect
+
+@export var sound_volume_label: Label
+@export var music_volume_label: Label
+
+@export var menu_music: AudioStreamPlayer
+@export var music_list: Array[AudioStreamWAV]
+
+var sfx := AudioServer.get_bus_index("SFX")
+var music := AudioServer.get_bus_index("Music")
+
+var _sfx_volume: float = 1.0
+var _music_volume: float = 1.0
+
+func _ready() -> void:
+	options_menu.visible = false
+	_sfx_volume = AudioServer.get_bus_volume_linear(sfx)
+	_music_volume =AudioServer.get_bus_volume_linear(music)
+	_reset_label_text()
+	menu_music.stream = music_list[0]
+	menu_music.play()
+
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
 
 func _on_start_button_pressed() -> void:
-	Errands.reset_errands()
-	Scenes.next = preload("res://ui/errand_picker.tscn")
-	Scenes.goto(preload("res://cutscenes/driving.tscn"))
+	if menu_music.stream != music_list[3]:
+		menu_music.stream = music_list[3]
+		menu_music.play()
+	#Errands.reset_errands()
+	#Scenes.next = preload("res://ui/errand_picker.tscn")
+	#Scenes.goto(preload("res://cutscenes/driving.tscn"))
 
 func _on_options_button_pressed() -> void:
-	pass # Replace with function body.
+	options_menu.visible = true
+
+func _on_options_back_button_pressed() -> void:
+	options_menu.visible = false
+
+func _on_sounds_louder_button_pressed() -> void:
+	if (_sfx_volume + 0.1 <= 1):
+		_sfx_volume = _sfx_volume + 0.1
+		_reset_label_text()
+
+func _on_sounds_quieter_button_pressed() -> void:
+	if (_sfx_volume - 0.1 >= 0):
+		_sfx_volume = _sfx_volume - 0.1
+		_reset_label_text()
+
+func _on_music_louder_button_pressed() -> void:
+	if (_music_volume + 0.1 <= 1):
+		_music_volume = _music_volume + 0.1
+		_reset_label_text()
+
+func _on_music_quieter_button_pressed() -> void:
+	if (_music_volume - 0.1 >= 0):
+		_music_volume = _music_volume - 0.1
+		_reset_label_text()
+
+func _reset_label_text() -> void:
+	AudioServer.set_bus_volume_linear(sfx, _sfx_volume)
+	AudioServer.set_bus_volume_linear(music, _music_volume)
+	sound_volume_label.text = "%d" % (_sfx_volume * 100)
+	music_volume_label.text = "%d" % (_music_volume * 100)
+
+func _on_audio_stream_player_finished() -> void:
+	if menu_music.stream == music_list[3]:
+		Errands.reset_errands()
+		Scenes.next = preload("res://ui/errand_picker.tscn")
+		Scenes.goto(preload("res://cutscenes/driving.tscn"))
+
+	elif menu_music.stream == music_list[0]:
+		menu_music.stream = music_list[1]
+	elif menu_music.stream == music_list[1]:
+		menu_music.stream = music_list[2]
+	elif menu_music.stream == music_list[2]:
+		menu_music.stream = music_list[1]
+	menu_music.play()
