756f24753260588c2e0f3299a915e21b90ed0334
buchegerb1704 <buchegerb1704@gmail.com>
2026-07-25 23:47:01 -0500

    Fly swatting minigame

--- classes/follow_mouse.gd
+++ classes/follow_mouse.gd
@@ -2,7 +2,7 @@ extends Control
 
 var mousepositoion: int
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	var mouse_pos: Vector2 = get_viewport().get_mouse_position()
 	var pivot_pos: Vector2 = mouse_pos - self.pivot_offset
 	self.set_position(pivot_pos)
---
+++ errands/fly_movement.gd
@@ -0,0 +1,24 @@
+extends StaticBody2D
+
+@export var fly_anim: AnimatedSprite2D
+
+var _velocity: Vector2 = Vector2(200,200)
+var _coin: Array[float] = [-1,1]
+
+func _ready() -> void:
+	var speed_x: float = randf_range(200, 600) * _coin.pick_random()
+	var speed_y: float = randf_range(200, 600) * _coin.pick_random()
+	_velocity = Vector2(speed_x, speed_y)
+
+func _physics_process(delta: float) -> void:
+	var collision_info: KinematicCollision2D = move_and_collide(_velocity * delta)
+	if collision_info:
+		_velocity = _velocity.bounce(collision_info.get_normal())
+
+func _on_fly_anim_animation_changed() -> void:
+	if fly_anim.animation == "splat":
+		_velocity = Vector2(0,0)
+
+func _on_fly_anim_animation_finished() -> void:
+	if fly_anim.animation == "splat":
+		self.queue_free()
