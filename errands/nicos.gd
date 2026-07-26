f39589aca17000ba7642616030db7cf9d81fb386
buchegerb1704 <buchegerb1704@gmail.com>
2026-07-26 01:22:21 -0500

    Add button overlay to Nicos

--- errands/nicos.gd
+++ errands/nicos.gd
@@ -1,19 +1,25 @@
 extends Errand
 
 @export var nicos_anim: AnimatedSprite2D
+@export var a_button_anim: AnimatedSprite2D
+@export var d_button_anim: AnimatedSprite2D
 
 var _punches_required: int = -1
 
 func _ready() -> void:
+	a_button_anim.visible = false
+	d_button_anim.visible = false
 	_punches_required = randi_range(5, 15)
 
 func _input(event: InputEvent) -> void:
 	if nicos_anim.is_playing(): return
 	if event.is_action_released("punch_left"):
 		nicos_anim.play("punch_left")
+		a_button_anim.play("button_a")
 		_hurt_nicos()
 	elif event.is_action_released("punch_right"):
 		nicos_anim.play("punch_right")
+		d_button_anim.play("button_d")
 		_hurt_nicos()
 
 func _hurt_nicos() -> void:
@@ -21,7 +27,10 @@ func _hurt_nicos() -> void:
 	_punches_required = _punches_required - 1
 
 func _on_nicos_anim_animation_finished() -> void:
-	if (nicos_anim.animation == "punch_left" or nicos_anim.animation == "punch_right") and _punches_required == 0:
+	if (nicos_anim.animation == "default"):
+		a_button_anim.visible = true
+		d_button_anim.visible = true
+	elif (nicos_anim.animation == "punch_left" or nicos_anim.animation == "punch_right") and _punches_required == 0:
 		nicos_anim.play("dizzy_windup")
 	elif (nicos_anim.animation == "dizzy_windup"):
 		#TODO -> Start knockout audio
